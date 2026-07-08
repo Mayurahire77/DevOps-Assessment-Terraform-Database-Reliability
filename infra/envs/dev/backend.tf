1ca7bb86334ca13990fa4acbf9136e0ad471bc90
Mayur <mayurahire15646@gmail.com>
2026-07-08 22:08:23 +0530

    first commit

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,19 +1,19 @@
 terraform {
 
-  backend "s3" {
+  #backend "s3" {
 
-    bucket = "your-terraform-state-bucket"
+#      bucket = "your-terraform-state-bucket"
 
-    key = "dev/terraform.tfstate"
+ #     key = "dev/terraform.tfstate"
 
-    region = "ap-south-1"
-
-
-    dynamodb_table = "terraform-locks"
+  #    region = "ap-south-1"
 
 
-    encrypt = true
+ #   dynamodb_table = "terraform-locks"
 
-  }
+
+ #   encrypt = true
+
+ # }
 
 }
